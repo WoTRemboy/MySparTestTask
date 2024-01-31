ac196c39945f610ecdf311a76867a20dbc4ae3c4
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-01-31 13:34:34 +0300

    Initial Commit

---
+++ MySparTestTask/MySparTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparTestTaskApp.swift
+//  MySparTestTask
+//
+//  Created by Roman Tverdokhleb on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
