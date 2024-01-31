609e3de62c3c90b189cd441816f3aa5892347e6f
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-01-31 14:09:16 +0300

    model sketches

--- MySparTestTask/Model/Model.swift
+++ MySparTestTask/Model/Model.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Product {
+    let id: String = UUID().uuidString
+    let name: String
+    let description: String
+    let image: Image
+    let homeland: Homeland
+    let characteristics: Characteristics
+    let reviews: [Review]
+    let price: Price
+}
+
+struct Homeland {
+    let contry: String
+    let town: String
+    let image: Image?
+}
+
+struct Characteristics {
+    let production: Homeland
+    let calories: String
+    let fats: String
+    let squirrels: String
+    let carbohydrates: String
+}
+
+struct Review {
+    let id: String = UUID().uuidString
+    let userName: String
+    let date: Date
+    let rating: Int
+    let description: String
+}
+
+struct Price {
+    let regularKgPrice: Int
+    let currentKgPrice: Int
+    let regularItemPrice: Int
+    let currentItemPrice: Int
+}
