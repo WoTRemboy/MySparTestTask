aeaf48c3a3b60e0891a2e6c663ad6f42758711b9
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-01-31 16:35:47 +0300

    navigation bar buttons added

--- MySparTestTask/View/MainView.swift
+++ MySparTestTask/View/MainView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct MainView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Environment(\.colorScheme) private var colorScheme: ColorScheme
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                }
+                .padding()
+            .navigationBarItems(leading: leadingNavButton, trailing: trailingNavButtons)
         }
-        .padding()
+        
+    }
+    
+    var leadingNavButton: some View {
+        Button(action: {}, label: {
+            Image.Navigation.arrowBack
+                .fontWeight(.medium)
+                .modifier(NavigationSetup(size: CGSize(width: 20, height: 25)))
+        })
+    }
+    
+    var trailingNavButtons: some View {
+        HStack {
+            Spacer()
+            Button(action: {}, label: {
+                Image.Navigation.check
+                    .resizable()
+                    .fontWeight(.medium)
+                    .modifier(NavigationSetup(size: CGSize(width: 20, height: 25)))
+            })
+            .padding(.trailing, 6)
+            
+            Button(action: {}, label: {
+                Image.Navigation.share
+                    .resizable()
+                    .fontWeight(.medium)
+                    .modifier(NavigationSetup(size: CGSize(width: 20, height: 25)))
+            })
+            .padding(.trailing, 6)
+            
+            Button(action: {}, label: {
+                Image.Navigation.heart
+                    .resizable()
+                    .fontWeight(.medium)
+                    .modifier(NavigationSetup(size: CGSize(width: 23, height: 23)))
+            })
+            .padding(.bottom, -4)
+        }
     }
 }
 
--- MySparTestTask/View/ViewModifiers.swift
+++ MySparTestTask/View/ViewModifiers.swift
@@ -5,4 +5,13 @@
 //  Created by Roman Tverdokhleb on 31.01.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct NavigationSetup: ViewModifier {
+    let size: CGSize
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(Color.IconColors.iconsForeground)
+            .frame(width: size.width, height: size.height)
+    }
+}
